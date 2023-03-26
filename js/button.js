9f938674dd10f6030f7a9f89ae48b7259ac8f9cb
Anton Maksimenkov <urbangorod888@gmail.com>
2023-03-26 15:00:36 +0300

    added burger menu

---
+++ js/button.js
@@ -0,0 +1,13 @@
+let menuBtnOpen = document.querySelectorAll(".button-js");
+let menuBtnClose = document.querySelector(".modal__menu__close-btn-js");
+let menu = document.querySelector(".b-modal__menu__container");
+
+for (let element of menuBtnOpen) {
+  element.addEventListener("click", function (event) {
+    menu.classList.toggle("active");
+  });
+}
+
+menuBtnClose.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
